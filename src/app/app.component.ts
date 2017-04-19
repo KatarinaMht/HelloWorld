35e96b380cad129e7b98ec1c35137f428a22d3cc
katkraus <katarina.kraus@mht.net>
2017-04-19 11:44:17 +0200

    First commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+  title = 'My First Angular App';
+
+  public closeAlert() {
+    console.log("call close alert");
+  }
+}
